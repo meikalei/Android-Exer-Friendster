042cbd4347bba271cb000797ac49aa9421d44694
Rain Tomista <rstomista@up.edu.ph>
2017-04-14 17:54:08 +0800

    Added project files

---
+++ app/src/main/java/io/acssuplb/github/friendster/Friend.java
@@ -0,0 +1,31 @@
+package io.acssuplb.github.friendster;
+
+public class Friend {
+    private int id;
+    private String name;
+
+    public Friend() {
+
+    }
+
+    public Friend(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
